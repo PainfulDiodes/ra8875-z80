c2d2100b422ce2d502bea6c8781de597e454a57b
Stephen Willcock <160266765+PainfulDiodes@users.noreply.github.com>
2026-03-25 07:04:21 +0000

    console: placeholder for backspace

--- asm/console.asm
+++ asm/console.asm
@@ -13,10 +13,12 @@
 ;   ra8875_console_cursor_y - set cursor row to A (0..RA8875_ROWS-1, logical)
 ;
 ; Special characters recognised by ra8875_console_putchar:
+;   0x08  BS  - backspace: substituted with '<' (test placeholder)
 ;   0x0a  LF  - newline: erase cursor, advance to next row (scrolls if needed)
 ;   0x0d  CR  - same as LF
 ;   0x0e  SO  - cursor on:  make software cursor visible
 ;   0x0f  SI  - cursor off: hide software cursor
+;   0x7f  DEL - backspace key on most terminals: substituted with '<' (test placeholder)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
     INCLUDE "ra8875.inc"
@@ -85,7 +87,13 @@ ra8875_console_putchar:
     jr z,_putchar_newline
     cp 0x0d                     ; carriage return? TODO - validate - do we need this?
     jr z,_putchar_newline
+    cp 0x08                     ; BS - backspace?
+    jr z,_putchar_backspace
+    cp 0x7f                     ; DEL - backspace key on most terminals?
+    jr z,_putchar_backspace
     ; character overwrites software cursor at current position; RA8875 auto-advances
+_putchar_backspace:
+    ld a,'<'                    ; substitute '<' as a test placeholder
     call ra8875_putchar
     ld hl,RA8875_CURSOR_COL
     inc (hl)
